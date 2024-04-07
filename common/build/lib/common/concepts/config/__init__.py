2eba48a882b5696ad59668052e56396e5935819f
Andrey Trokhachev <avtrokhachev@yandex-team.ru>
2024-04-07 18:43:27 +0300

    fix: linters

--- common/build/lib/common/concepts/config/__init__.py
+++ common/build/lib/common/concepts/config/__init__.py
@@ -1,2 +1,7 @@
 from common.concepts.config import constants
-from common.concepts.config.actions import get_config, get_value, set_config, set_value
+from common.concepts.config.actions import (
+    get_config,
+    get_value,
+    set_config,
+    set_value,
+)
--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-from api.api.concepts.steam_items.handlers import router
-
-app = FastAPI()
-app.include_router(router)
