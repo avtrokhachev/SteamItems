62a43ca972fb46b278bef9dde9c4b18b432d6917
Andrey Trokhachev <avtrokhachev@yandex-team.ru>
2024-04-10 12:37:55 +0300

    moved api to microservice

--- api/common/common/concepts/config/constants.py
+++ api/common/common/concepts/config/constants.py
@@ -2,7 +2,7 @@ import enum
 
 DEFAULT_CONFIGS_DIR = "/etc/configs"
 DEFAULT_CONFIG_NAME = "config.yaml"
-DEFAULT_DEV_CONFIG_DIR = "/Users/avtrokhachev/Documents/projects/SteamItems/configs"  # TODO: awful, fix
+DEFAULT_DEV_CONFIG_DIR = "/Users/avtrokhachev/Documents/projects/SteamItems/api/configs"  # TODO: awful, fix
 DEFAULT_DEV_CONFIG_NAME = "config.yaml"
 
 
